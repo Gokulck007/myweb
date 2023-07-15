f3ffb6b3ddbe52c5a033fe3f9e64acbb3a409b54
Gokulck007 <137037494+Gokulck007@users.noreply.github.com>
2023-07-15 19:37:24 +0530

    Add files via upload

---
+++ style.css
@@ -0,0 +1,10 @@
+document.getElementById("submitButton").addEventListener("click", function() {
+  var name = document.getElementById("nameInput").value;
+  var greeting = document.getElementById("greeting");
+
+  if (name) {
+    greeting.textContent = "Hello, " + name + "! Welcome to the web application.";
+  } else {
+    greeting.textContent = "Please enter your name.";
+  }
+});
